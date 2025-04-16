88900faaff21f9107bcadb39f60b8d2970803032
Arshad Saquib <arshad@ini8labs.tech>
2025-04-16 11:34:42 +0530

    fix issues

--- main.tf
+++ main.tf
@@ -59,14 +59,10 @@ resource "azurerm_linux_virtual_machine" "bastion" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
   size                = "Standard_B2s"
-  admin_username      = "azureuser"
+  admin_username      = "jaguar"
+  admin_password      = "Jaguar@1234"
   network_interface_ids = [azurerm_network_interface.bastion_nic.id]
-  disable_password_authentication = true
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
+  disable_password_authentication = false
 
   os_disk {
     caching              = "ReadWrite"
@@ -81,8 +77,19 @@ resource "azurerm_linux_virtual_machine" "bastion" {
   }
 }
 
+resource "azurerm_route_table" "aks_route_table" {
+  name                = "jaguar-aks-udr"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet_route_table_association" "aks_subnet_association" {
+  subnet_id      = azurerm_subnet.aks_subnet.id
+  route_table_id = azurerm_route_table.aks_route_table.id
+}
+
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "private-aks"
+  name                = "jaguar-private-aks"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "privateaks"
@@ -106,127 +113,3 @@ resource "azurerm_kubernetes_cluster" "aks" {
     outbound_type     = "userDefinedRouting"
   }
 }
-
-resource "azurerm_public_ip" "appgw_pip" {
-  name                = "appgw-pip"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_application_gateway" "appgw" {
-  name                = "appgateway"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  sku {
-    name     = "Standard_v2"
-    tier     = "Standard_v2"
-    capacity = 2
-  }
-
-  gateway_ip_configuration {
-    name      = "appgw-ipcfg"
-    subnet_id = azurerm_subnet.appgw_subnet.id
-  }
-
-  frontend_port {
-    name = "frontendPort"
-    port = 80
-  }
-
-  frontend_ip_configuration {
-    name                 = "frontendIP"
-    public_ip_address_id = azurerm_public_ip.appgw_pip.id
-  }
-
-  backend_address_pool {
-    name = "backendPool"
-  }
-
-  backend_http_settings {
-    name                  = "httpSettings"
-    cookie_based_affinity = "Disabled"
-    port                  = 80
-    protocol              = "Http"
-    request_timeout       = 30
-  }
-
-  http_listener {
-    name                           = "httpListener"
-    frontend_ip_configuration_name = "frontendIP"
-    frontend_port_name             = "frontendPort"
-    protocol                       = "Http"
-  }
-
-  request_routing_rule {
-    name                       = "rule1"
-    rule_type                  = "Basic"
-    http_listener_name         = "httpListener"
-    backend_address_pool_name  = "backendPool"
-    backend_http_settings_name = "httpSettings"
-  }
-}
-
-resource "azurerm_role_assignment" "agic" {
-  principal_id         = azurerm_kubernetes_cluster.aks.identity[0].principal_id
-  role_definition_name = "Contributor"
-  scope                = azurerm_application_gateway.appgw.id
-}
-
-resource "helm_release" "agic" {
-  name       = "agic"
-  repository = "https://appgwingress.blob.core.windows.net/ingress-azure-helm-package/"
-  chart      = "ingress-azure"
-  version    = "1.5.0"
-  namespace  = "default"
-  create_namespace = false
-
-  set {
-    name  = "appgw.name"
-    value = azurerm_application_gateway.appgw.name
-  }
-
-  set {
-    name  = "appgw.resourceGroup"
-    value = azurerm_resource_group.rg.name
-  }
-
-  set {
-    name  = "appgw.subscriptionId"
-    value = data.azurerm_client_config.current.subscription_id
-  }
-
-  set {
-    name  = "appgw.shared"
-    value = "false"
-  }
-
-  set {
-    name  = "armAuth.type"
-    value = "msi"
-  }
-
-  set {
-    name  = "armAuth.identityResourceID"
-    value = azurerm_kubernetes_cluster.aks.identity[0].identity_resource_id
-  }
-
-  set {
-    name  = "armAuth.identityClientID"
-    value = azurerm_kubernetes_cluster.aks.identity[0].client_id
-  }
-
-  set {
-    name  = "rbac.enabled"
-    value = "true"
-  }
-
-  set {
-    name  = "verbosityLevel"
-    value = "3"
-  }
-}
-
-data "azurerm_client_config" "current" {}
